c8aab17c907f76d7741d147ffa1f043c9e71491a
Suman599 <chakrabortysuman5999@gmail.com>
2024-10-25 18:32:05 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import './App.css'
-import RedirectLink from './pages/redirect-link'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import RedirectLink from './pages/redirect-link';
 import Auth from './pages/auth';
 import Link from './pages/link';
 import Dashboard from './pages/dashboard';
@@ -9,43 +9,48 @@ import AppLayout from './layouts/app-layout';
 import UrlProvider from './context';
 import RequireAuth from './components/require-auth';
 
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <LandingPage />
+      },
+      {
+        path: '/dashboard',
+        element: (
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>
+        ),
+      },
+      {
+        path: '/auth',
+        element: <Auth />
+      },
+      {
+        path: '/link/:id',
+        element: (
+          <RequireAuth>
+            <Link />
+          </RequireAuth>
+        ),
+      },
+      {
+        path: '/:id',
+        element: <RedirectLink />
+      }
+    ]
+  }
+]);
 
-
-  
-  const router=createBrowserRouter([
-    {
-      element:<AppLayout/>,
-      children:[
-        {
-          path:'/',
-          element:<LandingPage />
-        },
-        {
-          path:'/dashboard',
-          element:(<RequireAuth><Dashboard /></RequireAuth>),
-        },
-        {
-          path:'/auth',
-          element:<Auth />
-        },
-        {
-          path:'/link/:id',
-          element:(<RequireAuth><Link /></RequireAuth>),
-        },
-        {
-          path:'/:id',
-          element:<RedirectLink />
-        }
-      ]
-    }
-  ]);
-
-  function App(){
-  return(
+function App() {
+  return (
     <UrlProvider>
-    <RouterProvider router={router} />;
+      <RouterProvider router={router} />
     </UrlProvider>
   );
-  }
+}
 
 export default App;
