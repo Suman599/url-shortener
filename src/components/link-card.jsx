be01899ba3f938c709012207fd48b6f3f8cffa7d
Suman599 <chakrabortysuman5999@gmail.com>
2024-10-29 10:40:24 +0530

    mess

--- src/components/link-card.jsx
+++ src/components/link-card.jsx
@@ -30,7 +30,7 @@ const LinkCard = ({ url, fetchUrls }) => {
             <Link to={`/link/${url?.id}`} className="flex flex-col flex-1">
                 <span className="text-3xl font-extrabold hover:underline cursor-pointer">{url?.title}</span>
                 <span className="text-2xl text-blue-400 font-bold hover:underline cursor-pointer">
-                    https://trimrr.in/{url?.custom_url ? url.custom_url : url.short_url}
+                    https://trimrr-ten.vercel.app/{url?.custom_url ? url.custom_url : url.short_url}
                 </span>
                 <span className="flex items-center gap-1 hover:underline cursor-pointer">{url?.original_url}</span>
                 <span className="flex items-end font-extralight text-sm flex-1">{new Date(url?.created_at).toLocaleString()}</span>
@@ -38,7 +38,7 @@ const LinkCard = ({ url, fetchUrls }) => {
             <div className="flex gap-2">
                 <Button 
                     variant="ghost" 
-                    onClick={() => navigator.clipboard.writeText(`https://trimrr.in/${url?.short_url}`)}
+                    onClick={() => navigator.clipboard.writeText(`https://trimrr-ten.vercel.app/${url?.short_url}`)}
                 >
                     <Copy />
                 </Button>
